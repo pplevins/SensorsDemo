699f3f8af4c8fe99a7face15e9d1bce833421bea
Pinni Plevinski <shareytora007@gmail.com>
2025-06-18 18:02:06 +0300

    Adding infrastructure layer, mainly for mysql base preperation.

---
+++ SensorsDemo/Infrastructure/InMemoryPlayerRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace SensorsDemo.Infrastructure;
+
+/// <summary>
+/// Class to give in-memory infrastructure to save historical data for players in the game.
+/// </summary>
+internal sealed class InMemoryPlayerRepository : IPlayerRepository
+{
+    private readonly Dictionary<string, string> _db = [];
+
+    // <inheritdoc>
+    public void SaveHighestRank(string playerId, string rank) => _db[playerId] = rank;
+    // <inheritdoc>
+    public string? GetHighestRank(string playerId) => _db.TryGetValue(playerId, out var r) ? r : null;
+}
